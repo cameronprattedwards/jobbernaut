50d5d8debc5a9644cfb08d8734ddd16398d3adee
Cameron Edwards <cameronprattedwards@gmail.com>
2014-08-16 11:18:09 -0400

    Add jshint task

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp"),
+	jshint = require("gulp-jshint");
+
+gulp.task("jshint", function () {
+	return gulp.src([
+			"./App/**/*.js",
+			"./Data/**/*.js",
+			"./Domain/**/*.js",
+			"./Utils/**.*.js"
+		])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
